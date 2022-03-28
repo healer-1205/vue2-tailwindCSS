7fb53a8dd67998935e8f3c8b4b35c2534d7e7b25
healer-1205 <crazyhealer1205@gmail.com>
2022-03-29 01:17:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="text-gray-900 flex flex-col min-h-screen bg-gray-200">
+    <div class="flex-shrink-0 py-12 text-center">
+      <Hello></Hello>
+      <div class="mt-2">
+        <a href="https://github.com/james2doyle/vue-tailwind-starter" class="text-sm font-semibold text-gray-600 hover:underline" v-text="message"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from 'Components/Hello.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      message: 'Source on GitHub'
+    };
+  },
+};
+</script>
+
+<style>
+  @import './styles/style.css';
+</style>
